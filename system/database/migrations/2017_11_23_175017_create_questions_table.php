1b03e83ca0e121dcdd63056e9051b2d732f253ec
La Treta <yves_henry13@hotmail.com>
2018-04-29 00:05:58 -0300

    fixing table questions

--- system/database/migrations/2017_11_23_175017_create_questions_table.php
+++ system/database/migrations/2017_11_23_175017_create_questions_table.php
@@ -16,6 +16,7 @@ class CreateQuestionsTable extends Migration
         Schema::create('questions', function (Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->string('unique_id');
+            $table->integer('store_onwer')->unsigned();
             $table->integer('product_id')->unsigned();
             $table->integer('ask_id')->unsigned();
             $table->string('question', 150);
@@ -26,6 +27,8 @@ class CreateQuestionsTable extends Migration
             ->references('id')->on('products');
             $table->foreign('ask_id')
             ->references('id')->on('users');
+            $table->foreign('store_owner')
+            ->references('id')->on('stores');
             // Datas
             $table->timestamp('answered_at');
             $table->timestamp('created_at')->useCurrent();
